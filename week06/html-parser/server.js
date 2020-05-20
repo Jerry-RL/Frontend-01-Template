bfb4c8c3e02e29beed30be9018f9483b7f42b903
Jerry-RL <mr.rare001@gmail.com>
2020-05-20 23:59:14 +0800

    第六周初始化

---
+++ week06/html-parser/server.js
@@ -0,0 +1,31 @@
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type','text/html')
+    res.setHeader('X-Foo', 'bar')
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+
+    res.end(
+`
+<html maaa=a >
+<head>
+    <style>
+body div #myid{
+    width: 100px;
+    background: #ff5000;
+}
+body div img{
+    width: 30px;
+    background: #ff1111;
+}
+    </style>
+</head>
+<body>
+    <div>
+        <img id="myid" />
+        <img />
+    </div>
+</body>
+`
+    )
+})
